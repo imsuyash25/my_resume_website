85e2b66da79054289bd4bbfbd93e56739f10f65d
imsuyash25 <asatisuyash@gmail.com>
2023-01-23 15:28:44 +0530

    added hash router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 import './styles.css';
-import Navbar from './navbar.js';
 import Contact from './contact.js';
 import Home from './home';
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import {HashRouter as Router , Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div classNameName="App">
     <Router>
         <Routes>
-            <Route path ="/" element ={<Home/>}/>
+            <Route path ="/home" element ={<Home/>}/>
             <Route path="/contact" element={<Contact/>}/>
         </Routes>
     </Router>
