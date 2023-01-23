d4d0018eee155c63ba008df14192a833ad73b4a9
imsuyash25 <asatisuyash@gmail.com>
2023-01-23 14:30:34 +0530

    added contact page

--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {useState} from 'react';
-import './navbar_style.css';
+import './navbar_style.css'
+import {Link} from 'react-router-dom';
 
-
-function Navbar(){
+export function Navbar(){
     const [toggle, setTog] = useState(false);
 
     function handle(e){
@@ -22,7 +22,7 @@ function Navbar(){
                 <div className={toggle? "mobile-menu-link" :"menu-link"}>
                     <ul>
                         <li>
-                            <a href="#Home"> Home</a>
+                            <Link to="/"> Home</Link>
                         </li>
                         <li>
                             <a href="#Projects">Projects</a>
@@ -34,7 +34,7 @@ function Navbar(){
                             <a href="#AboutMe"> About</a>
                         </li>
                         <li>
-                            <a href="#ContactMe"> Contact</a>
+                            <Link to="/contact">Contact</Link>
                         </li>
                     </ul>
                 </div>
@@ -42,11 +42,23 @@ function Navbar(){
 
                 <div className="hamburger-menu">
                     <a href="#" onClick={handle} >
-                    <img src="https://t3.ftcdn.net/jpg/01/09/45/80/240_F_109458015_QsWmchlzuwCZPqIUWR7HcTDsbbptejRv.jpg"/>
+                    <img src="https://t3.ftcdn.net/jpg/01/09/45/80/240_F_109458015_QsWmchlzuwCZPqIUWR7HcTDsbbptejRv.jpg" alt=""/>
                     </a>
                 </div>
         </nav>
         </>
     )
 }
-export default Navbar;
+
+export function Contactbar(){
+    return(
+        <>
+        <nav className ="main-nav">
+            <div className ="logo">
+                <h2><span>S</span>UYASH 
+                <span>A</span>SATI</h2>
+            </div>
+        </nav>
+        </>
+    );
+}
