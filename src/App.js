f7ddc05750056191394516d14a49fa512245af91
imsuyash25 <asatisuyash@gmail.com>
2023-01-23 15:43:44 +0530

    addeD

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import './styles.css';
 import Contact from './contact.js';
 import Home from './home';
-import {HashRouter as Router , Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div classNameName="App">
-    <Router>
+    <Router basename="/my_resume_website">
         <Routes>
-            <Route path ="/home" element ={<Home/>}/>
+            <Route path ="/" element ={<Home/>}/>
             <Route path="/contact" element={<Contact/>}/>
         </Routes>
     </Router>
